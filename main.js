756de3109a5128a88068f3b5c7d7058a81560ea7
Elie Rouphael <elie.rouphael@polymtl.ca>
2018-11-14 09:57:38 -0500

    WIP :tests!

--- main.js
+++ main.js
@@ -10,4 +10,10 @@ exampleSocket.onmessage = function(event){
 	var msg = JSON.parse(event.data);
 	console.log(msg);
 }
-
+function test(){
+	var now = new Date();
+	var message = new Message("onGetChannel","dbf646dc-5006-4d9f-8815-fd37514818ee","this is a test","me",now);
+	//var message =  {"eventType":"onMessage","channelId":"dbf646dc-5006-4d9f-8815-fd37514818ee","data":"this is a test","sender":"me","timestamp":now};
+	var test = JSON.stringify(message);
+	exampleSocket.send(test);
+}
