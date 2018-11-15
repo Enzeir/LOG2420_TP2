3a645fb0e51b519866d9cc04c24f0580d27d69c7
Pierre Le Henaff <lehenaffpierre92@gmail.com>
2018-11-15 08:11:10 -0500

    worked on the channel boards interface

--- main.js
+++ main.js
@@ -15,5 +15,5 @@ function test(){
 	var now = new Date();
 	var message = new Message("onGetChannel","dbf646dc-5006-4d9f-8815-fd37514818ee","this is a test","me",now);
 	var test = JSON.stringify(message);
-	exampleSocket.send(test);
+	websocket.send(test);
 }
